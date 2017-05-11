83d8732ff87212c4b7ed29c291f2e604cefb2252
Dave Stensgaard <info@devdave.com>
2017-05-10 20:11:48 -0700

    initial commit

---
+++ GreedKata/Greed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedKata
+{
+    public class Greed
+    {
+        public int Score(List<int> dice)
+        {
+            return 0;
+        }
+    }
+}
